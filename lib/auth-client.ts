672c3e1ec2a01ac2253223c3269b60a36fec9af0
Yacine1605 <bouziryacine1@gmail.com>
2025-04-08 00:39:46 +0100

    v2.0.0

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,7 @@ import { usernameClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://mrigal.vercel.app/",
 
   plugins: [usernameClient()],
 });
