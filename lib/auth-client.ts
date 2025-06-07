c0b94cfbeb0ac953958055945f93ebe8555626ed
mrigal16 <mrigal.digitservz16@gmail.com>
2025-06-07 16:22:16 +0100

    Update auth-client.ts

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,7 +3,10 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: process.env.BETTER_AUTH_URL,
-
+ trustedOrigins: [
+    "http://localhost:3000",           // for dev
+    "https://mrigal.digitservz.dz",    // your Vercel frontend
+  ],
   plugins: [usernameClient()],
 });
 export const {
