b2968bdbde095f8b62c9d7ddc8dea1089807bf99
mrigal16 <mrigal.digitservz16@gmail.com>
2025-07-06 14:59:20 +0100

    conatc

--- components/SendEmail.tsx
+++ components/SendEmail.tsx
@@ -16,9 +16,9 @@ const SendEmail = async ({
   const __dirname = path.dirname(__filename);
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-     port: process.env.PORT,
-  secure: true, // true for port 465, false for other ports
-  auth: {
+    port: process.env.PORT,
+    secure: true, // true for port 465, false for other ports
+    auth: {
     user: process.env.EMAIL_MAIN, // generated ethereal user
     pass: process.env.EMAIL_PASSWORD,
     },
